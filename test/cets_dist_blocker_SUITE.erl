dfa6add130e09dbafe40e718d75b629701686e6e
Mikhail Uvarov <arcusfelis@gmail.com>
2024-03-06 11:55:27 +0100

    Add dist_blocker_unblocks_if_cleaner_says_done_and_second_cleaner_goes_down

--- test/cets_dist_blocker_SUITE.erl
+++ test/cets_dist_blocker_SUITE.erl
@@ -14,6 +14,7 @@ all_cases() ->
         dist_blocker_waits_for_cleaning,
         dist_blocker_unblocks_if_cleaner_goes_down,
         dist_blocker_unblocks_if_cleaner_goes_down_and_second_cleaner_says_done,
+        dist_blocker_unblocks_if_cleaner_says_done_and_second_cleaner_goes_down,
         dist_blocker_skip_blocking_if_no_cleaners,
         unknown_down_message_is_ignored,
         unknown_message_is_ignored,
@@ -87,6 +88,23 @@ dist_blocker_unblocks_if_cleaner_goes_down_and_second_cleaner_says_done(Config) 
     pang = net_adm:ping(Node2),
     erlang:exit(Cleaner, killed),
     cets_dist_blocker:cleaning_done(self(), Node2),
+    %% Connection is unblocked
+    pong = net_adm:ping(Node2),
+    gen_server:stop(Blocker).
+
+dist_blocker_unblocks_if_cleaner_says_done_and_second_cleaner_goes_down(Config) ->
+    #{peer_ct2 := Node2} = proplists:get_value(nodes, Config),
+    {ok, Blocker} = cets_dist_blocker:start_link(),
+    %% Two cleaners
+    cets_dist_blocker:add_cleaner(self()),
+    Cleaner = spawn_cleaner(),
+    pong = net_adm:ping(Node2),
+    true = erlang:disconnect_node(Node2),
+    %% Connection is blocked
+    pang = net_adm:ping(Node2),
+    %% Different order comparing to dist_blocker_unblocks_if_cleaner_goes_down_and_second_cleaner_says_done
+    cets_dist_blocker:cleaning_done(self(), Node2),
+    erlang:exit(Cleaner, killed),
     %% Connection is unblocked
     pong = net_adm:ping(Node2),
     gen_server:stop(Blocker).
