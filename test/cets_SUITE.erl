a87e6d537259c321501f60485ba682dacfd7de9f
Mikhail Uvarov <arcusfelis@gmail.com>
2024-03-12 16:44:20 +0100

    Make cets_join_SUITE

--- test/cets_SUITE.erl
+++ test/cets_SUITE.erl
@@ -97,14 +97,7 @@ cases() ->
         insert_many_with_two_records,
         delete_works,
         delete_many_works,
-        join_works,
         inserted_records_could_be_read_back_from_replicated_table,
-        join_works_with_existing_data,
-        join_works_with_existing_data_with_conflicts,
-        join_works_with_existing_data_with_conflicts_and_defined_conflict_handler,
-        join_works_with_existing_data_with_conflicts_and_defined_conflict_handler_and_more_keys,
-        join_works_with_existing_data_with_conflicts_and_defined_conflict_handler_and_keypos2,
-        bag_with_conflict_handler_not_allowed,
         bag_with_conflict_handler_not_allowed_for_start_link,
         insert_new_works,
         insert_new_works_with_table_name,
@@ -125,12 +118,6 @@ cases() ->
         insert_serial_works_when_leader_is_back,
         insert_serial_blocks_when_leader_is_not_back,
         leader_is_the_same_in_metadata_after_join,
-        join_with_the_same_pid,
-        join_ref_is_same_after_join,
-        join_fails_because_server_process_not_found,
-        join_fails_because_server_process_not_found_before_get_pids,
-        join_fails_before_send_dump,
-        join_fails_before_send_dump_and_there_are_pending_remote_ops,
         send_dump_fails_during_join_because_receiver_exits,
         join_fails_in_check_fully_connected,
         join_fails_because_join_refs_do_not_match_for_nodes_in_segment,
@@ -307,9 +294,6 @@ delete_many_works(Config) ->
     cets:insert(Tab, {alice, 32}),
     cets:delete_many(Tab, [alice]),
     [] = ets:lookup(Tab, alice).
-
-join_works(Config) ->
-    given_two_joined_tables(Config).
 
 inserted_records_could_be_read_back_from_replicated_table(Config) ->
     #{tab1 := Tab1, tab2 := Tab2} = given_two_joined_tables(Config),
@@ -661,207 +645,6 @@ leader_is_the_same_in_metadata_after_join(Config) ->
     Leader = cets:get_leader(Pid2),
     Leader = cets_metadata:get(T1, leader),
     Leader = cets_metadata:get(T2, leader).
-
-join_works_with_existing_data(Config) ->
-    Tab1 = make_name(Config, 1),
-    Tab2 = make_name(Config, 2),
-    {ok, Pid1} = start_local(Tab1),
-    {ok, Pid2} = start_local(Tab2),
-    cets:insert(Tab1, {alice, 32}),
-    %% Join will copy and merge existing tables
-    ok = cets_join:join(lock_name(Config), #{}, Pid1, Pid2),
-    [{alice, 32}] = ets:lookup(Tab2, alice).
-
-%% This testcase tests an edgecase: inserting with the same key from two nodes.
-%% Usually, inserting with the same key from two different nodes is not possible
-%% (because the node-name is a part of the key).
-join_works_with_existing_data_with_conflicts(Config) ->
-    Tab1 = make_name(Config, 1),
-    Tab2 = make_name(Config, 2),
-    {ok, Pid1} = start_local(Tab1),
-    {ok, Pid2} = start_local(Tab2),
-    cets:insert(Tab1, {alice, 32}),
-    cets:insert(Tab2, {alice, 33}),
-    %% Join will copy and merge existing tables
-    ok = cets_join:join(lock_name(Config), #{}, Pid1, Pid2),
-    %% We insert data from other table into our table when merging, so the values get swapped
-    [{alice, 33}] = ets:lookup(Tab1, alice),
-    [{alice, 32}] = ets:lookup(Tab2, alice).
-
-join_works_with_existing_data_with_conflicts_and_defined_conflict_handler(Config) ->
-    Opts = #{handle_conflict => fun resolve_highest/2},
-    Tab1 = make_name(Config, 1),
-    Tab2 = make_name(Config, 2),
-    {ok, Pid1} = start_local(Tab1, Opts),
-    {ok, Pid2} = start_local(Tab2, Opts),
-    cets:insert(Tab1, {alice, 32}),
-    cets:insert(Tab2, {alice, 33}),
-    %% Join will copy and merge existing tables
-    ok = cets_join:join(lock_name(Config), #{}, Pid1, Pid2),
-    %% Key with the highest Number remains
-    [{alice, 33}] = ets:lookup(Tab1, alice),
-    [{alice, 33}] = ets:lookup(Tab2, alice).
-
-join_works_with_existing_data_with_conflicts_and_defined_conflict_handler_and_more_keys(Config) ->
-    %% Deeper testing of cets_join:apply_resolver function
-    Opts = #{handle_conflict => fun resolve_highest/2},
-    #{tabs := [T1, T2, T3], pids := [Pid1, Pid2, Pid3]} = given_3_servers(Config, Opts),
-    cets:insert_many(T1, [{alice, 32}, {bob, 10}, {michal, 40}]),
-    cets:insert_many(T2, [{alice, 33}, {kate, 3}, {michal, 2}]),
-    %% Join will copy and merge existing tables
-    ok = cets_join:join(lock_name(Config), #{}, Pid1, Pid2),
-    ok = cets_join:join(lock_name(Config), #{}, Pid1, Pid3),
-    %% Key with the highest Number remains
-    Dump = [{alice, 33}, {bob, 10}, {kate, 3}, {michal, 40}],
-    Dump = cets:dump(T1),
-    Dump = cets:dump(T2),
-    Dump = cets:dump(T3).
-
--record(user, {name, age, updated}).
-
-%% Test with records (which require keypos = 2 option)
-join_works_with_existing_data_with_conflicts_and_defined_conflict_handler_and_keypos2(Config) ->
-    Opts = #{handle_conflict => fun resolve_user_conflict/2, keypos => 2},
-    T1 = make_name(Config, 1),
-    T2 = make_name(Config, 2),
-    {ok, Pid1} = start_local(T1, Opts),
-    {ok, Pid2} = start_local(T2, Opts),
-    cets:insert(T1, #user{name = alice, age = 30, updated = erlang:system_time()}),
-    cets:insert(T2, #user{name = alice, age = 25, updated = erlang:system_time()}),
-    %% Join will copy and merge existing tables
-    ok = cets_join:join(keypos2_lock, #{}, Pid1, Pid2),
-    %% Last inserted record is in the table
-    [#user{age = 25}] = ets:lookup(T1, alice),
-    [#user{age = 25}] = ets:lookup(T2, alice).
-
-%% Keep record with highest timestamp
-resolve_user_conflict(U1 = #user{updated = TS1}, _U2 = #user{updated = TS2}) when
-    TS1 > TS2
-->
-    U1;
-resolve_user_conflict(_U1, U2) ->
-    U2.
-
-resolve_highest({K, A}, {K, B}) ->
-    {K, max(A, B)}.
-
-bag_with_conflict_handler_not_allowed(Config) ->
-    {error, [bag_with_conflict_handler]} =
-        cets:start(make_name(Config), #{handle_conflict => fun resolve_highest/2, type => bag}).
-
-bag_with_conflict_handler_not_allowed_for_start_link(Config) ->
-    {error, [bag_with_conflict_handler]} =
-        cets:start_link(make_name(Config), #{handle_conflict => fun resolve_highest/2, type => bag}).
-
-join_with_the_same_pid(Config) ->
-    Tab = make_name(Config),
-    {ok, Pid} = start_local(Tab),
-    %% Just insert something into a table to check later the size
-    cets:insert(Tab, {1, 1}),
-    link(Pid),
-    {error, join_with_the_same_pid} = cets_join:join(lock_name(Config), #{}, Pid, Pid),
-    Nodes = [node()],
-    %% The process is still running and no data loss (i.e. size is not zero)
-    #{nodes := Nodes, size := 1} = cets:info(Pid).
-
-join_ref_is_same_after_join(Config) ->
-    #{pid1 := Pid1, pid2 := Pid2} = given_two_joined_tables(Config),
-    #{join_ref := JoinRef} = cets:info(Pid1),
-    #{join_ref := JoinRef} = cets:info(Pid2).
-
-join_fails_because_server_process_not_found(Config) ->
-    {ok, Pid1} = start_local(make_name(Config, 1)),
-    {ok, Pid2} = start_local(make_name(Config, 2)),
-    F = fun
-        (join_start) ->
-            exit(Pid1, sim_error);
-        (_) ->
-            ok
-    end,
-    {error, {task_failed, {noproc, {gen_server, call, [Pid1, get_info, infinity]}}, _}} =
-        cets_join:join(lock_name(Config), #{}, Pid1, Pid2, #{checkpoint_handler => F}).
-
-join_fails_because_server_process_not_found_before_get_pids(Config) ->
-    {ok, Pid1} = start_local(make_name(Config, 1)),
-    {ok, Pid2} = start_local(make_name(Config, 2)),
-    F = fun
-        (before_get_pids) ->
-            exit(Pid1, sim_error);
-        (_) ->
-            ok
-    end,
-    {error, {task_failed, {noproc, {gen_server, call, [Pid1, other_servers, infinity]}}, _}} =
-        cets_join:join(lock_name(Config), #{}, Pid1, Pid2, #{checkpoint_handler => F}).
-
-join_fails_before_send_dump(Config) ->
-    Me = self(),
-    DownFn = fun(#{remote_pid := RemotePid, table := _Tab}) ->
-        Me ! {down_called, self(), RemotePid}
-    end,
-    {ok, Pid1} = start_local(make_name(Config, 1), #{handle_down => DownFn}),
-    {ok, Pid2} = start_local(make_name(Config, 2), #{}),
-    cets:insert(Pid1, {1}),
-    cets:insert(Pid2, {2}),
-    F = fun
-        ({before_send_dump, P}) when Pid1 =:= P ->
-            Me ! before_send_dump_called_for_pid1;
-        ({before_send_dump, P}) when Pid2 =:= P ->
-            error(sim_error);
-        (_) ->
-            ok
-    end,
-    ?assertMatch(
-        {error, {task_failed, sim_error, #{}}},
-        cets_join:join(lock_name(Config), #{}, Pid1, Pid2, #{checkpoint_handler => F})
-    ),
-    %% Ensure we sent dump to Pid1
-    receive_message(before_send_dump_called_for_pid1),
-    %% Not joined, some data exchanged
-    cets:ping_all(Pid1),
-    cets:ping_all(Pid2),
-    [] = cets:other_pids(Pid1),
-    [] = cets:other_pids(Pid2),
-    %% Pid1 applied new version of dump
-    %% Though, it got disconnected after
-    {ok, [{1}, {2}]} = cets:remote_dump(Pid1),
-    %% Pid2 rejected changes
-    {ok, [{2}]} = cets:remote_dump(Pid2),
-    receive_message({down_called, Pid1, Pid2}).
-
-%% Checks that remote ops are dropped if join_ref does not match in the state and in remote_op message
-join_fails_before_send_dump_and_there_are_pending_remote_ops(Config) ->
-    Me = self(),
-    {ok, Pid1} = start_local(make_name(Config, 1)),
-    {ok, Pid2} = start_local(make_name(Config, 2)),
-    F = fun
-        ({before_send_dump, P}) when Pid1 =:= P ->
-            Me ! before_send_dump_called_for_pid1;
-        ({before_send_dump, P}) when Pid2 =:= P ->
-            sys:suspend(Pid2),
-            error(sim_error);
-        (before_unpause) ->
-            %% Crash in before_unpause, otherwise cets_join will block in cets:unpause/2
-            %% (because Pid2 is suspended).
-            %% Servers would be unpaused automatically though, because cets_join process exits
-            %% (i.e. cets:unpause/2 call is totally optional)
-            error(sim_error2);
-        (_) ->
-            ok
-    end,
-    ?assertMatch(
-        {error, {task_failed, sim_error2, #{}}},
-        cets_join:join(lock_name(Config), #{}, Pid1, Pid2, #{checkpoint_handler => F})
-    ),
-    %% Ensure we sent dump to Pid1
-    receive_message(before_send_dump_called_for_pid1),
-    cets:insert_request(Pid1, {1}),
-    %% Check that the remote_op has reached Pid2 message box
-    cets_test_wait:wait_for_remote_ops_in_the_message_box(Pid2, 1),
-    sys:resume(Pid2),
-    %% Wait till remote_op is processed
-    cets:ping(Pid2),
-    %% Check that the insert was ignored
-    {ok, []} = cets:remote_dump(Pid2).
 
 send_dump_fails_during_join_because_receiver_exits(Config) ->
     Me = self(),
